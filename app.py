cd662059d932eb0970a047cba467305f20ff0b55
Ezekiel Owolabi <84450428+Deejoft@users.noreply.github.com>
2026-03-17 11:09:13 +0100

    Fix return message in home route

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Deployment Successful!
+    return " Pipeline Deployment Successful!
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
