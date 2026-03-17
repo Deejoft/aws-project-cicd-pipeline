89dd1aa3c351bb56b519e27deb13fe86a3abd69e
Ezekiel Owolabi <84450428+Deejoft@users.noreply.github.com>
2026-03-17 11:07:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Deployment Successful!"
-    return "Hello"
+    return "CI/CD Pipeline Deployment Successful!
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
